5d4897e8a938eda4caa7532e2e770d30a8eb366b
Hayden Woolstenhulme <hwools@Haydens-MacBook-Air-2.local>
2018-09-06 23:11:41 -0600

    set up test image slideshow

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -2,16 +2,37 @@ import React, {Component} from 'react';
 
 
 class Slideshow extends Component {
+    
+    componentDidMount(){
+        this.setCarouselImg();
+    }
     constructor(){
         super();
         this.state = {
-            images: []
+            images: ['https://buzzsharer.com/wp-content/uploads/2016/10/corgi-photo.jpg', 'https://collegian.com/wp-content/uploads/2017/09/093017_APOTTS_AC_TourdeCorgi-9.jpg', 'https://assets3.thrillist.com/v1/image/2444807/size/tmg-article_tall;jpeg_quality=20.jpg'],
+            currentImage: '0'
         }
+        this.setCarouselImg = this.setCarouselImg.bind(this);
+    }
+
+
+    setCarouselImg(){
+        let i =0;
+        setInterval(()=>{
+            i++;
+       if(i === 3){
+          i=0;
+       }
+       this.setState({currentImage:i})
+    }, 5000);
     }
 
     render(){
+        
         return(
-            <div>Hey</div>
+            <div>
+                <img src={this.state.images[this.state.currentImage]} width="500px" height="350px" alt=""/>
+            </div>
         )
     }
 }
