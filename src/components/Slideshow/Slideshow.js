316ef2e62334cc343cf39c292657235a823f7cfb
Hayden Woolstenhulme <hwools@Haydens-MacBook-Air-2.local>
2018-09-07 19:32:39 -0600

    styled buttons added current image dots

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+// eslint-disable-next-line
+import slideshow from './slideshow.css';
 
 class Slideshow extends Component {
 
@@ -10,8 +11,8 @@ class Slideshow extends Component {
         
         super();
         this.state = {
-            images: ['https://buzzsharer.com/wp-content/uploads/2016/10/corgi-photo.jpg', 'https://collegian.com/wp-content/uploads/2017/09/093017_APOTTS_AC_TourdeCorgi-9.jpg', 'https://assets3.thrillist.com/v1/image/2444807/size/tmg-article_tall;jpeg_quality=20.jpg', 'https://uproxx.files.wordpress.com/2016/09/loki-the-corgi.jpg?quality=95&w=650', 'https://static.boredpanda.com/blog/wp-content/uploads/2018/05/funny-corgis-cute-dogs-42-5afd687db521e__700.jpg', 'http://i.imgur.com/WMlHCpH.jpg'],
-            currentImage: '0'
+            images: ['https://buzzsharer.com/wp-content/uploads/2016/10/corgi-photo.jpg', 'https://collegian.com/wp-content/uploads/2017/09/093017_APOTTS_AC_TourdeCorgi-9.jpg', 'https://assets3.thrillist.com/v1/image/2444807/size/tmg-article_tall;jpeg_quality=20.jpg', 'https://uproxx.files.wordpress.com/2016/09/loki-the-corgi.jpg?quality=95&w=650', 'https://static.boredpanda.com/blog/wp-content/uploads/2018/05/funny-corgis-cute-dogs-42-5afd687db521e__700.jpg', 'http://i.imgur.com/WMlHCpH.jpg', 'http://www.bravotv.com/sites/nbcubravotv/files/styles/blog-post--mobile/public/field_blog_image/2018/04/unleashed-corgi-people-promo.jpg?itok=mKp8i56f&timestamp=1524257239'],
+            currentImage: 0
         }
         this.setCarouselImg = this.setCarouselImg.bind(this);
         this.moveCarouselBackward = this.moveCarouselBackward.bind(this);
@@ -52,15 +53,30 @@ class Slideshow extends Component {
          
        }
        this.setState({currentImage:i})
-    }, 5000);
+    }, 7000);
     }
 
     render(){
         
         return(
             <div>
-                <button onClick={this.moveCarouselBackward}>Back</button><img src={this.state.images[this.state.currentImage]} width="500px" height="350px" alt=""/><button onClick={this.moveCarouselForward}>Forward</button>
-                <p>{this.state.currentImage}</p>
+            <div className='flex-container-carousel'>
+                <button onClick={this.moveCarouselBackward} className="backbutton"></button>
+                    <img className="overlay"/>
+                    <img src={this.state.images[this.state.currentImage]} alt="" className="carouselimg"/>
+                <button onClick={this.moveCarouselForward} className="forwardbutton"></button>
+                </div>
+                <div className="dots-flex">
+                <span class="dot1" dot-selected={this.state.currentImage === 0 ? 'on' : 'off'}></span>
+                <span class="dot2" dot-selected={this.state.currentImage === 1 ? 'on' : 'off'}></span>
+                <span class="dot3" dot-selected={this.state.currentImage === 2 ? 'on' : 'off'}></span>
+                <span class="dot4" dot-selected={this.state.currentImage === 3 ? 'on' : 'off'}></span>
+                <span class="dot5" dot-selected={this.state.currentImage === 4 ? 'on' : 'off'}></span>
+                <span class="dot6" dot-selected={this.state.currentImage === 5 ? 'on' : 'off'}></span>
+                <span class="dot7" dot-selected={this.state.currentImage === 6 ? 'on' : 'off'}></span>
+                </div>
+            
+            {/* <p>{this.state.currentImage}</p> */}
             </div>
         )
     }
