3fde5f957bb5e69c0ef788e82f456667539694e5
Hayden Woolstenhulme <hwoolstenhulme@hwoolstenhulme-.corp.instructure.com>
2018-12-28 20:29:07 -0700

    product page CSS + Get product info

--- src/components/Productpage/Product.js
+++ src/components/Productpage/Product.js
@@ -1,28 +1,43 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Navbar from '../Navbar/Navbar';
+import product from './product.css';
 
  class Product extends Component {
+
+    componentDidMount(){
+        this.getProduct();
+    }
    
     constructor(){
         super();
         this.state ={
             product: []
         }
+        this.getProduct = this.getProduct.bind(this);
     }
 
 
     getProduct(){
-        let productcode = this.props.match.params
+        let productcode = this.props.match.params.productcode
+        axios.get(`/api/product/${productcode}`).then((req)=> {
+            this.setState({product: req.data[0]})
+        })
     }
 
     render(){
         return(
-            
             <div>
                 <Navbar/>
-                <div className="productcontainer">
-                    More things
+                <div className="singleproductflex">
+                    <div className="singleproductcontainer">
+                        <div className="singleproductimg">
+                            <img src={this.state.product.image} alt={this.state.product.productname} height="auto" width="auto"></img>
+                        </div>
+                        <div className="singleproductname">
+                            <h1>{this.state.product.productname}</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
