c4cf714bb105a542b9dfc15204dabb2ca7417965
Hayden Woolstenhulme <hwoolstenhulme@hwoolstenhulme-.corp.instructure.com>
2019-01-17 18:25:25 -0700

    added quantity and add to cart + styling

--- src/components/Productpage/Product.js
+++ src/components/Productpage/Product.js
@@ -8,13 +8,18 @@ import product from './product.css';
     componentDidMount(){
         this.getProduct();
     }
+    componentWillMount(){
+        this.topOfPage();
+    }
    
     constructor(){
         super();
         this.state ={
-            product: []
+            product: [],
+            quantity: ""
         }
         this.getProduct = this.getProduct.bind(this);
+        this.topOfPage = this.topOfPage.bind(this);
     }
 
 
@@ -24,8 +29,17 @@ import product from './product.css';
             this.setState({product: req.data[0]})
         })
     }
+    topOfPage(){
+            window.scrollTo(0, 0);
+    }
 
     render(){
+        const {
+            quantity
+        } = this.state;
+        const updateByPropertyName = (propertyName, value)  => ({
+            [propertyName]: value
+          });
         return(
             <div>
                 <Navbar/>
@@ -34,9 +48,18 @@ import product from './product.css';
                         <div className="singleproductimg">
                             <img src={this.state.product.image} alt={this.state.product.productname} height="auto" width="auto"></img>
                         </div>
-                        <div className="singleproductname">
-                            <h1>{this.state.product.productname}</h1>
+                        <div className="productdetails">
+                            <div className="singleproductname">
+                                <h1>{this.state.product.productname}</h1>
+                            </div>
+                            <div className="singleproductprice">
+                            <div className="singleline">
+                                <p>${this.state.product.price}</p>
+                               <input type="text" value={this.state.quantity}  maxLength="4" onChange={event => this.setState(updateByPropertyName('quantity', event.target.value))}></input><button>Add To Cart</button>
+                               </div>
+                            </div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
