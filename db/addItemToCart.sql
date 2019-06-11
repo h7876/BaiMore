fa4377cc38c7a3a4e16c3d6648ffdc90bda99e76
Hayden Woolstenhulme <hayd1234@gmail.com>
2019-06-11 13:43:54 -0600

    added ordering by time/date added in cart

--- db/addItemToCart.sql
+++ db/addItemToCart.sql
@@ -3,6 +3,6 @@ WITH upsert AS (
      WHERE cartid=$2 AND productcode=$3 
      RETURNING *
 )
-INSERT INTO cart (cartid, productcode, quantity) 
-SELECT $2, $3, $1
+INSERT INTO cart (cartid, productcode, quantity, dateadded) 
+SELECT $2, $3, $1, current_timestamp
 WHERE NOT EXISTS (SELECT * FROM upsert)
--- db/getSingleUserCart.sql
+++ db/getSingleUserCart.sql
@@ -1,1 +1,1 @@
-select p.productname, p.productcode, p.price, p.image, c.quantity from cart c inner join products p on c.productcode = p.productcode where c.cartid = $1
+select p.productname, p.productcode, p.price, p.image, c.quantity, dateadded from cart c inner join products p on c.productcode = p.productcode where c.cartid = $1 order by dateadded asc;
